e99b08e20770908efbd019468146b4f03d994d64
Marian Hlavacka <91958835+majohlavacka@users.noreply.github.com>
2023-05-05 23:09:49 +0200

    Delete inc/login directory

--- inc/login/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-     unset($_SESSION["user_name"]);
-     unset($_SESSION["user_email"]); 
-     echo 'Boli ste odhlásení';
-    //session_destroy(); 
-    header('URL = index.php');
-
-?>
